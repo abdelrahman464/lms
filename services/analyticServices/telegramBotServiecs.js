254a1f0cde82e85e7b9c502585e6e64a8594fdae
Abd_El Rahman Mohamed <apdomedo6@gmail.com>
2023-07-06 19:05:29 +0300

    telegram

--- services/analyticServices/telegramBotServiecs.js
+++ services/analyticServices/telegramBotServiecs.js
@@ -2,9 +2,9 @@ const TelegramBot = require("node-telegram-bot-api");
 const TelegramBotMessage = require("../../models/analyticModels/analyticTelgramBotModel");
 const factory = require("../handllerFactory");
 
-// Add a new message
+// Add a new message channel 1
 const addMessage = async () => {
-  const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
+  const bot = new TelegramBot(process.env.TELEGRAM_TOKEN1, {
     polling: true,
   });
   bot.on("message", async (msg) => {
@@ -16,6 +16,20 @@ const addMessage = async () => {
   });
 };
 addMessage();
+// Add a new message channel 2
+const addMessage2 = async () => {
+  const bot = new TelegramBot(process.env.TELEGRAM_TOKEN2, {
+    polling: true,
+  });
+  bot.on("message", async (msg) => {
+    await TelegramBotMessage.create({
+      chatId: msg.chat.id,
+      messageId: msg.message_id,
+      text: msg.text,
+    });
+  });
+};
+addMessage2();
 //@desc get all messaege From bot
 //@route GET api/v1/analytic/telegram
 //@access protected user,admin
