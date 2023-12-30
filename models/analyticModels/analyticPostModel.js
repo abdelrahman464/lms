3fea5819e056060eb2a4e17f126d44c8769bfe79
Abdo gomaa <abdogomaa2700@gmail.com>
2023-12-30 16:27:26 +0200

    convert public to home

--- models/analyticModels/analyticPostModel.js
+++ models/analyticModels/analyticPostModel.js
@@ -15,8 +15,8 @@ const analyticPostSchema = new mongoose.Schema(
     },
     sharedTo: {
       type: String,
-      enum: ["public","analytic","weeklyWithdraw"],
-      default: "public",
+      enum: ["home","analytic","weeklyWithdraw"],
+      default: "home",
     },
     image: String,
   },
